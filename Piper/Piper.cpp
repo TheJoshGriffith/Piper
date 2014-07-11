1e1bfcec4eaebeddc1afe2401590c28e1af54586
Joshua Griffith <josh@joshgriffith.co.uk>
2014-07-11 23:47:35 +0100

    Added some process processing shit

--- Piper/Piper.cpp
+++ Piper/Piper.cpp
@@ -2,29 +2,50 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
+#include <Psapi.h>
 #include "Piper.h"
 
 #define BUFFER_SIZE 512
 
 Piper::Piper()
 {
+	// Get tibias main module for future use
+	getModule();
+	// Define a pipe name
 	pipeName = L"\\\\.\\pipe\\pipeName";
+	// Create the pipe
 	hPipe = CreateNamedPipe(pipeName, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE, 1, BUFFER_SIZE, BUFFER_SIZE, 5000, NULL);
+	// If we didn't make it, tell the user something went wrong.
 	if (hPipe == NULL || hPipe == INVALID_HANDLE_VALUE)
 	{
 		MessageBox(NULL, L"Error loading named pipe, please consult your software provider for support", L"Error!", MB_ICONSTOP);
 	}
+	// Wait for a client to connect to see the data down the pipe.
 	ConnectNamedPipe(hPipe, NULL);
-	wchar_t buff[BUFFER_SIZE] = L"SYSTEM OPERATIONAL!";
-	WriteFile(hPipe, buff, sizeof(buff), &lastPacketSize, NULL);
+	// Send a message down the pipe to confirm it's working
+	Send(L"SYSTEM OPERATIONAL");
 }
 
 Piper::~Piper()
 {
+	// Clean up after ourselves by closing the pipe handle (and releasing it for future use)
 	CloseHandle(hPipe);
 }
 
 void Piper::Send(wchar_t * msg)
 {
 	WriteFile(hPipe, msg, sizeof(msg), &lastPacketSize, NULL);
+}
+
+void Piper::getModule()
+{
+	DWORD cbNeeded;
+	HANDLE pHandle = GetCurrentProcess();
+	HMODULE hMods[1024];
+	// Currently the first enumerated module will always be Tibia_exe.
+	if (EnumProcessModules(pHandle, hMods, sizeof(hMods), &cbNeeded))
+	{
+		baseAddress = (DWORD) hMods[0];
+	}
 }
--- Piper/Piper.h
+++ Piper/Piper.h
@@ -7,9 +7,11 @@ public:
 	Piper();
 	~Piper();
 	void Send(wchar_t * msg);
+	void getModule();
 
 	// Variables
 	static HANDLE hPipe;
 	static wchar_t *pipeName;
 	DWORD lastPacketSize;
+	DWORD baseAddress;
 };
