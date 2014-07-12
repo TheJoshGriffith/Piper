c0d51736b2db6f6fef96b21f41444ae24f85586d
XtrmJosh <josh@joshgriffith.co.uk>
2014-07-12 16:36:37 +0100

    Fixed injector, now just running the exe will inject the dll and what not.

--- PipeeWPF/MainWindow.xaml.cs
+++ PipeeWPF/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.IO;
 using System.IO.Pipes;
 using System.Threading;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace PipeeWPF
 {
@@ -31,9 +32,14 @@ namespace PipeeWPF
         {
             InitializeComponent();
 
-            DllInjector dllinj = new DllInjector();
+            int pid = Process.GetProcessesByName("Tibia")[0].Id;
+            
+            bool res = InjectDLL(pid);
 
-            dllinj.Inject("Tibia.exe", "Piper.dll");
+            if (!res)
+            {
+                MessageBox.Show("Failed to inject!");
+            }
 
             listenerThread = new Thread(new ThreadStart(addToList));
             startupThread = new Thread(new ThreadStart(initNpcs));
@@ -68,6 +74,41 @@ namespace PipeeWPF
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             listenerThread.Abort();
+            startupThread.Abort();
+        }
+
+        private bool InjectDLL(int processId)
+        {
+            string DLL_PATH = "C:\\Users\\Joshua\\Documents\\visual studio 2013\\Projects\\Piper\\PipeeWPF\\bin\\Debug\\Piper.dll";
+            uint DLL_PATH_LENGTH = (uint)DLL_PATH.Length;
+            IntPtr kernel = WinAPI.GetModuleHandle("Kernel32");
+            IntPtr loadLibrary = WinAPI.GetProcAddress(kernel, "LoadLibraryA");
+
+            if (loadLibrary == IntPtr.Zero)
+                return false;
+
+            IntPtr process = WinAPI.OpenProcess(WinAPI.PROCESS_ALL_ACCESS, false, processId);
+
+            if (process == IntPtr.Zero)
+                return false;
+
+            IntPtr remoteMemory = WinAPI.VirtualAllocEx(process, IntPtr.Zero, DLL_PATH_LENGTH + 1, WinAPI.AllocationType.Commit, WinAPI.MemoryProtection.ReadWrite);
+
+            if (remoteMemory == IntPtr.Zero)
+            {
+                WinAPI.CloseHandle(process);
+                return false;
+            }
+
+            //byte[] bytes = Encoding.ASCII.GetBytes(DLL_PATH);
+            UIntPtr lpNumberOfBytesWritten;
+            WinAPI.WriteProcessMemory(process, remoteMemory, Encoding.ASCII.GetBytes(DLL_PATH), DLL_PATH_LENGTH, out lpNumberOfBytesWritten);
+            IntPtr remoteThread = WinAPI.CreateRemoteThread(process, IntPtr.Zero, 0, loadLibrary, remoteMemory, 0, IntPtr.Zero);
+
+            WinAPI.VirtualFreeEx(process, remoteMemory, DLL_PATH_LENGTH, WinAPI.AllocationType.Release);
+            WinAPI.CloseHandle(process);
+
+            return true;
         }
     }
 }
---
+++ PipeeWPF/WinAPI.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PipeeWPF
+{
+    class WinAPI
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress,
+           uint dwSize, AllocationType dwFreeType);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+            [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess,
+           IntPtr lpThreadAttributes, uint dwStackSize, IntPtr
+           lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Int32 WaitForSingleObject(IntPtr Handle, UInt32 Wait);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+           uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        public const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+        public const int PROCESS_CREATE_THREAD = 0x0002;
+        public const int PROCESS_QUERY_INFORMATION = 0x0400;
+        public const int PROCESS_VM_READ = 0x0010;
+        public const int PROCESS_VM_WRITE = 0x0020;
+        public const int PROCESS_VM_OPERATION = 0x008;
+        public const int PROCESS_SUSPEND_RESUME = 0x0800;
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+    }
+}
